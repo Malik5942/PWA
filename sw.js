0ff04e06fcdbc3e8ce3146c52fa6979e41395663
Mudassir Malik <46223724+Malik5942@users.noreply.github.com>
2019-02-19 12:23:10 +0500

    Testing Progressive Web App

---
+++ sw.js
@@ -0,0 +1,49 @@
+var cacheName = 'PWA-cache-10'
+var filesToCache = [
+    './',
+    '/index.html',
+    '/script.js',
+    '/style.css',
+    "/gallary.html",
+    '/about.html',
+    '/404.html',
+    '/img-1.jpg',
+    '/img-2.jpg',
+    '/img-3.jpg',
+    '/img-4.jpg',
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== cacheName) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+    );
+    return self.clients.claim();
+  });
+
+
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  });
